35bdf310da0d56c2e133f99379e41908f95dfa47
jtnkmr03 <63794764+jtnkmr03@users.noreply.github.com>
2020-06-10 02:21:21 +0530

    commit

--- Plot3.R
+++ Plot3.R
@@ -15,4 +15,3 @@ lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 dev.off()
-© 2020 GitHub, Inc.
